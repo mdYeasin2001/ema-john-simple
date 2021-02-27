55bdd688079db7cf854ca4c5e876ac7c9f4aa710
Yeasin <yeasin30112001@gmail.com>
2021-02-27 02:13:35 -0600

    learn to generate fake json data and use on projects

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import GeneratedData from './Data/data.json';
 
 function App() {
+  const [data, setData] = useState([]);
+  console.log(data);
+  useEffect(() => {
+    setData(GeneratedData);
+  }, [])
   return (
     <div>
       <Header/>

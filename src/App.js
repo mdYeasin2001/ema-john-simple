fdc4c6a0655f00c8faad72f5e28461990479c0f3
Yeasin <yeasin30112001@gmail.com>
2021-03-03 23:21:34 -0600

    Learn to add react router on react project and explore a little

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
 import GeneratedData from './Data/data.json';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Review from './components/Review/Review';
+import Inventory from './components/Inventory/Inventory';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   const [data, setData] = useState([]);
@@ -12,8 +21,27 @@ function App() {
   }, [])
   return (
     <div>
-      <Header/>
-      <Shop/>
+      <Header />
+
+      <Router>
+        <Switch>
+          <Route path="/shop">
+            <Shop />
+          </Route>
+          <Route path="/review">
+            <Review/>
+          </Route>
+          <Route path="/manage">
+            <Inventory/>
+          </Route>
+          <Route exact path="/">
+            <Shop/>
+          </Route>
+          <Route path="*">
+            <NotFound/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Inventory/Inventory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Inventory = () => {
+    return (
+        <div>
+            <h1>Developer is sleeping. It will come soon...</h1>
+        </div>
+    );
+};
+
+export default Inventory;
